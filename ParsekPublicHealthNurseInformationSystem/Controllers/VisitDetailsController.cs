8660132964acdd533f8da013e30c138ab680cdcc
venom1270 <zigsim1@gmail.com>
2017-04-29 13:56:12 +0200

    Created visit details view and controller, improved workorder frontend

---
+++ ParsekPublicHealthNurseInformationSystem/Controllers/VisitDetailsController.cs
@@ -0,0 +1,32 @@
+﻿using ParsekPublicHealthNurseInformationSystem.Models;
+using ParsekPublicHealthNurseInformationSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParsekPublicHealthNurseInformationSystem.Controllers
+{
+    [AuthorizationFilter(Role.RoleEnum.Employee)]
+    public class VisitDetailsController : Controller
+    {
+        private EntityDataModel DB = new EntityDataModel();
+
+        public ActionResult Index(int? visitId)
+        {
+            VisitDetailsViewModel vm = new VisitDetailsViewModel();
+
+            if (visitId == null)
+            {
+                vm.Visit = null;
+            }
+            else
+            {
+                vm.Visit = DB.Visits.Find(visitId);
+            }
+           
+            return View(vm);
+        }
+    }
+}
---
+++ ParsekPublicHealthNurseInformationSystem/ViewModels/Visit/VisitDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParsekPublicHealthNurseInformationSystem.ViewModels
+{
+    public class VisitDetailsViewModel
+    {
+        public Models.Visit Visit { get; set; }
+    }
+}
