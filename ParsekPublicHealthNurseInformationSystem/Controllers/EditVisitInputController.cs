7508c1a6526e50fc3167b81abfc715a0f455aa3d
venom1270 <zigsim1@gmail.com>
2017-05-24 17:19:11 +0200

    First version of visit input editor.

---
+++ ParsekPublicHealthNurseInformationSystem/Controllers/EditVisitInputController.cs
@@ -0,0 +1,114 @@
+﻿using ParsekPublicHealthNurseInformationSystem.Models;
+using ParsekPublicHealthNurseInformationSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParsekPublicHealthNurseInformationSystem.Controllers
+{
+    public class EditVisitInputController : Controller
+    {
+
+        private EntityDataModel DB = new EntityDataModel();
+
+        public ActionResult Index(EditVisitInputViewModel vm, int? serviceId)
+        {
+            if (vm == null)
+            {
+                vm = new EditVisitInputViewModel();
+                vm.SelectedVisitType = -1;
+            }
+
+            if (serviceId != null && vm.SelectedVisitType <= 0)
+            {
+                vm.SelectedVisitType = (int)serviceId;
+                return Index(vm, null);
+            }
+            
+            vm.ActivityInputList = new List<ActivityInput>();
+            vm.InputActivityList = new List<Activity>();
+
+            vm.VisitTypesList = DB.Services.ToList();
+            
+            
+            if (vm.SelectedVisitType != null && vm.SelectedVisitType > 0)
+            {
+                vm.ActivityInputList = DB.ActivityInputs.Where(ai => ai.Activity.Service.ServiceId == vm.SelectedVisitType &&
+                                                                     ai.InputType == ActivityInput.InputTypeEnum.Number)
+                                                                     .ToList();
+
+                vm.InputActivityList = DB.Services.Find(vm.SelectedVisitType).Activities.ToList();
+            }
+
+
+            return View(vm);
+        }
+
+        public ActionResult DeleteInput(int? visitInputId, int serviceId)
+        {
+
+            if (visitInputId != null)
+            {
+                try
+                {
+                    Models.ActivityInput AI = DB.ActivityInputs.Find(visitInputId);
+
+                    // Do we need this?
+                    AI.Activity = null;
+
+                    for (int i = 0; i < AI.ActivityInputDatas.Count; i++)
+                    {
+                        DB.ActivityInputDatas.Remove(AI.ActivityInputDatas.ElementAt(i));
+                    }
+                    
+
+                    DB.ActivityInputs.Remove(AI);
+                    DB.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    int x = 10;
+                }
+            }
+
+            return RedirectToAction("Index", new {  serviceId = serviceId });
+        }
+
+        public ActionResult AddInput(EditVisitInputViewModel vm)
+        {
+            try
+            {
+                if (vm.SelectedInputActivity > 0)
+                {
+                    Models.ActivityInput AI = new ActivityInput();
+                    AI.Activity = DB.Activities.Find(vm.SelectedInputActivity);
+                    AI.InputType = ActivityInput.InputTypeEnum.Number;
+                    AI.OneTime = false;
+                    AI.Required = vm.InputRequired;
+                    AI.Title = vm.InputTitle;
+                    string possibleValues = vm.InputMinValue + ";;" + vm.InputMaxValue;
+                    AI.PossibleValues = possibleValues;
+                    DB.ActivityInputs.Add(AI);
+                    DB.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                //ERROR
+                int x = 10;
+            }
+
+            vm.SelectedInputActivity = -1;
+            vm.InputMaxValue = 10000;
+            vm.InputMinValue = 0;
+            vm.InputRequired = false;
+            vm.InputTitle = "";
+
+
+            return RedirectToAction("Index", new { serviceId = vm.SelectedVisitType });
+        }
+    }
+}
---
+++ ParsekPublicHealthNurseInformationSystem/ViewModels/EditVisitInput/EditVisitInputViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ParsekPublicHealthNurseInformationSystem.ViewModels
+{
+    public class EditVisitInputViewModel
+    {
+
+        [Display(Name = "Izberi vrsto obiska")]
+        public List<Models.Service> VisitTypesList { get; set; }
+        public int SelectedVisitType { get; set; }
+
+        public List<Models.ActivityInput> ActivityInputList { get; set; }
+
+
+
+        //new input
+        [Display(Name = "Izberi kategorijo vnosa")]
+        public List<Models.Activity> InputActivityList { get; set; }
+        public int SelectedInputActivity { get; set; }
+
+        [Display(Name = "Naziv vnosa")]
+        public string InputTitle { get; set; }
+
+        [Display(Name = "Obvezen vnos")]
+        public bool InputRequired { get; set; }
+
+        [Display(Name = "Najmanjša vrednost")]
+        public int InputMinValue { get; set; }
+
+        [Display(Name = "Najvišja vrednost")]
+        public int InputMaxValue { get; set; }
+
+    }
+}
