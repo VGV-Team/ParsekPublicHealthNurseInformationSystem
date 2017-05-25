54664a3c24b4c20b80e557d9a98750de7ef7c882
Nejc Smrkolj Koželj <NEJCSK@hotmail.com>
2017-05-25 23:54:09 +0200

    Added logic for adding, editing and deleting code tables.

--- ParsekPublicHealthNurseInformationSystem/Controllers/CodeTableController.cs
+++ ParsekPublicHealthNurseInformationSystem/Controllers/CodeTableController.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 using Microsoft.Ajax.Utilities;
 using ParsekPublicHealthNurseInformationSystem.Models;
-using ParsekPublicHealthNurseInformationSystem.ViewModels.NurseReplacement;
-using ParsekPublicHealthNurseInformationSystem.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using ParsekPublicHealthNurseInformationSystem.ViewModels;
 
 namespace ParsekPublicHealthNurseInformationSystem.Controllers
@@ -50,50 +42,183 @@ namespace ParsekPublicHealthNurseInformationSystem.Controllers
                 default:
                     return View("Index", ctvm);
             }
-
             return View("Index", ctvm);
         }
 
-        public ActionResult Edit(CodeTableViewModel.CodeCategory category, int id)
+        public ActionResult Edit(CodeTableViewModel.CodeCategory category, int? id)
         {
             CodeTableViewModel ctvm = new CodeTableViewModel();
             switch (category)
             {
                 case CodeTableViewModel.CodeCategory.Medicine:
-                    ctvm.Medicine = DB.Medicines.FirstOrDefault(x => x.MedicineId == id);
-                    if(ctvm.Medicine == null)
-                        return RedirectToAction("Index");
+                    if (id == null)
+                        ctvm.Medicine = new Medicine();
+                    else
+                        ctvm.Medicine = DB.Medicines.FirstOrDefault(x => x.MedicineId == id);
                     break;
                 case CodeTableViewModel.CodeCategory.Contractor:
-                    ctvm.Contractor = DB.Contractors.FirstOrDefault(x => x.ContractorId == id);
-                    if (ctvm.Contractor == null)
-                        return RedirectToAction("Index");
+                    if (id == null)
+                        ctvm.Contractor = new Contractor();
+                    else
+                        ctvm.Contractor = DB.Contractors.FirstOrDefault(x => x.ContractorId == id); // TODO: add possible post offices!!
                     break;
                 case CodeTableViewModel.CodeCategory.Disease:
-                    ctvm.Disease = DB.Diseases.FirstOrDefault(x => x.DiseaseId == id);
-                    if (ctvm.Disease == null)
-                        return RedirectToAction("Index");
+                    if (id == null)
+                        ctvm.Disease = new Disease();
+                    else
+                        ctvm.Disease = DB.Diseases.FirstOrDefault(x => x.DiseaseId == id);
                     break;
                 case CodeTableViewModel.CodeCategory.Relationship:
-                    ctvm.Relationship = DB.Relationships.FirstOrDefault(x => x.RelationshipId == id);
-                    if (ctvm.Relationship == null)
-                        return RedirectToAction("Index");
+                    if (id == null)
+                        ctvm.Relationship = new Relationship();
+                    else
+                        ctvm.Relationship = DB.Relationships.FirstOrDefault(x => x.RelationshipId == id);
                     break;
                 case CodeTableViewModel.CodeCategory.Service:
-                    return RedirectToAction("Index");
+                    if (id == null)
+                        ctvm.Service = new Service();
+                    else
+                        ctvm.Service = DB.Services.FirstOrDefault(x => x.ServiceId == id);
+                    break;
             }
-
-            return View("Edit", ctvm);
+            return View("Index", ctvm);
         }
 
         public ActionResult Delete(CodeTableViewModel.CodeCategory category, int id)
         {
-            return null;
+            CodeTableViewModel ctvm = new CodeTableViewModel();
+            switch (category)
+            {
+                case CodeTableViewModel.CodeCategory.Medicine:
+                    ctvm.SelectedCodeCategory = CodeTableViewModel.CodeCategory.Medicine;
+                    Medicine medicine = DB.Medicines.FirstOrDefault(x => x.MedicineId == id);
+                    if(medicine != null)
+                        DB.Medicines.Remove(medicine);
+                    break;
+                case CodeTableViewModel.CodeCategory.Contractor:
+                    ctvm.SelectedCodeCategory = CodeTableViewModel.CodeCategory.Contractor;
+                    Contractor contractor = DB.Contractors.FirstOrDefault(x => x.ContractorId == id);
+                    if (contractor != null)
+                        DB.Contractors.Remove(contractor);
+                    break;
+                case CodeTableViewModel.CodeCategory.Disease:
+                    ctvm.SelectedCodeCategory = CodeTableViewModel.CodeCategory.Disease;
+                    Disease disease = DB.Diseases.FirstOrDefault(x => x.DiseaseId == id);
+                    if (disease != null)
+                        DB.Diseases.Remove(disease);
+                    break;
+                case CodeTableViewModel.CodeCategory.Relationship:
+                    ctvm.SelectedCodeCategory = CodeTableViewModel.CodeCategory.Relationship;
+                    Relationship relationship = DB.Relationships.FirstOrDefault(x => x.RelationshipId == id);
+                    if (relationship != null)
+                        DB.Relationships.Remove(relationship);
+                    break;
+                case CodeTableViewModel.CodeCategory.Service:
+                    ctvm.SelectedCodeCategory = CodeTableViewModel.CodeCategory.Service;
+                    Service service = DB.Services.FirstOrDefault(x => x.ServiceId == id);
+                    if (service != null)
+                        DB.Services.Remove(service);
+                    break;
+            }
+            DB.SaveChanges();
+            return RedirectToAction("Index", ctvm);
         }
 
-        public ActionResult New(CodeTableViewModel.CodeCategory category)
+        public ActionResult Save(CodeTableViewModel.CodeCategory category, CodeTableViewModel ctvm)
         {
-            return null;
+            if (ctvm == null)
+                return RedirectToAction("Index");
+
+            switch (category)
+            {
+                case CodeTableViewModel.CodeCategory.Medicine:
+                    if (ctvm.Medicine != null)
+                    {
+                        Medicine medicine = DB.Medicines.FirstOrDefault(x => x.MedicineId == ctvm.Medicine.MedicineId);
+                        if (medicine == null)
+                        {
+                            DB.Medicines.Add(ctvm.Medicine);
+                        }
+                        else
+                        {
+                            medicine.Title = ctvm.Medicine.Title;
+                            medicine.Code = ctvm.Medicine.Code;
+                        }
+                        DB.SaveChanges();
+                    }
+                    break;
+                case CodeTableViewModel.CodeCategory.Contractor:
+                    if (ctvm.Contractor != null)
+                    {
+                        Contractor contractor = DB.Contractors.FirstOrDefault(x => x.ContractorId == ctvm.Contractor.ContractorId);
+                        if (contractor == null)
+                        {
+                            DB.Contractors.Add(ctvm.Contractor);
+                        }
+                        else
+                        {
+                            contractor.Number = ctvm.Contractor.Number;
+                            contractor.Title = ctvm.Contractor.Title;
+                            contractor.Address = ctvm.Contractor.Address;
+                            contractor.PostOffice = DB.PostOffices.FirstOrDefault(x => x.PostOfficeId == ctvm.Contractor.PostOffice.PostOfficeId);
+                        }
+                        DB.SaveChanges();
+                    }
+                    break;
+                case CodeTableViewModel.CodeCategory.Disease:
+                    if (ctvm.Disease != null)
+                    {
+                        Disease disease = DB.Diseases.FirstOrDefault(x => x.DiseaseId == ctvm.Disease.DiseaseId);
+                        if (disease == null)
+                        {
+                            DB.Diseases.Add(ctvm.Disease);
+                        }
+                        else
+                        {
+                            disease.Code = ctvm.Disease.Code;
+                            disease.Description = ctvm.Disease.Description;
+                        }
+                        DB.SaveChanges();
+                    }
+                    break;
+                case CodeTableViewModel.CodeCategory.Relationship:
+                    if (ctvm.Relationship != null)
+                    {
+                        Relationship relationship = DB.Relationships.FirstOrDefault(x => x.RelationshipId == ctvm.Relationship.RelationshipId);
+                        if (relationship == null)
+                        {
+                            DB.Relationships.Add(ctvm.Relationship);
+                        }
+                        else
+                        {
+                            relationship.Name = ctvm.Relationship.Name;
+                        }
+                        DB.SaveChanges();
+                    }
+                    break;
+                case CodeTableViewModel.CodeCategory.Service:
+                    if (ctvm.Service != null)
+                    {
+                        Service service = DB.Services.FirstOrDefault(x => x.ServiceId == ctvm.Service.ServiceId);
+                        if (service == null)
+                        {
+                            DB.Services.Add(ctvm.Service);
+                        }
+                        else
+                        {
+                            service.ServiceCode = ctvm.Service.ServiceCode;
+                            service.ServiceTitle = ctvm.Service.ServiceTitle;
+                            service.PreventiveVisit = ctvm.Service.PreventiveVisit;
+                            service.RequiresMedicine = ctvm.Service.RequiresMedicine;
+                            service.RequiresBloodSample = ctvm.Service.RequiresBloodSample;
+                            service.RequiresPatients = ctvm.Service.RequiresPatients;
+                        }
+                        DB.SaveChanges();
+                    }
+                    break;
+            }
+            DB.SaveChanges();
+            return RedirectToAction("Index", ctvm);
         }
     }
 }
